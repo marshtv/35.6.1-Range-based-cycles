35bd13e74c77410d0e5008f01fa34e1a423b8952
marshtv <marshtv.1972@gmail.com>
2023-11-18 20:14:05 +0300

    Complete

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+	std::vector<int> numbers = {1, 2, 3, 4, 5};
+	for (auto number :  numbers)
+		std::cout << number << " ";
+	std::cout << std::endl;
+}
